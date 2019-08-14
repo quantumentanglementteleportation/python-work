1429e9cea8750f4939447ce5515a2bdad82e2f32
sumit <sumit@localhost.localdomain>
2019-08-14 17:06:45 +0530

    SSH tunnel to systemc

---
+++ setup.py
@@ -0,0 +1,28 @@
+import paramiko
+from sshtunnel import SSHTunnelForwarder
+
+with SSHTunnelForwarder(
+    # jump host IP and cred
+    ('192.168.43.44', 22),
+    ssh_username="xxxxx",
+    ssh_password="xxxxx",
+    # System C details
+    remote_bind_address=('192.168.4.202', 22),
+    local_bind_address=('0.0.0.0', 10022)
+) as tunnel:
+    client = paramiko.SSHClient()
+    #client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # username and password of system C
+    client.connect('127.0.0.1', 10022, username='xxxx', password='xxxx')
+    # do some operations with client session
+    stdin,stdout,stderr = client.exec_command('ls -l')
+    outlines=stdout.readlines()
+    resp=''.join(outlines)
+    print(resp)
+    #print(check)
+    #client.invoke_shell
+    client.close()
+
+print('FINISH!')
+
